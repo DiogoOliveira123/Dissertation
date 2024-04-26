4b6829d8ae4a61e8f7ce99ff9ff60917c4dcb87f
DiogoOliveira123 <126777024+DiogoOliveira123@users.noreply.github.com>
2024-04-26 16:38:01 +0100

    Delete Data Processing directory

--- Data Processing/main.py
+++
@@ -1,9 +0,0 @@
-from create_dataset import *
-
-if __name__ == '__main__':
-    NUM_PARTICIPANTS = 15
-    TEST_PARTICIPANTS = ['15', '08', '03']
-    VAL_PARTICIPANTS = ['02', '11']
-
-    data = Dataset()
-    data.SplitDataset(NUM_PARTICIPANTS, TEST_PARTICIPANTS, VAL_PARTICIPANTS)
