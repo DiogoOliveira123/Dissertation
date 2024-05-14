2e451d456e9ba8f9803703d82cfb56a74d3c7801
DiogoOliveira123 <126777024+DiogoOliveira123@users.noreply.github.com>
2024-05-14 18:54:23 +0100

    Changes is create_dataset.py and new python file.

--- GaitEventDetection/create_dataset.py
+++ GaitEventDetection/create_dataset.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-import random
 import re
 from tqdm import tqdm
 import cv2
@@ -8,6 +7,12 @@ import numpy as np
 
 
 def getFolders(directory, num):
+    """
+    Used in Dataset.CreateDataset() to split the train, val and test paths.
+    :param directory: base path to train, val and test directories.
+    :param num: number referring to train, val or test (0, 1, 2)
+    :return: path of train, val or test
+    """
     contents = os.listdir(directory)
     path = []
 
@@ -24,7 +29,7 @@ def getFolders(directory, num):
 class Dataset:
     def __init__(self):
         self.dataset_path = r'C:\Users\diman\OneDrive\Ambiente de Trabalho\DATASET'
-        self.excel_name = 'RGB_labeling_30Hz_balanced_aligned_v2.xlsx'
+        self.excel_name = 'RGB_labeling_30Hz_balanced_aligned_v4.xlsx'
 
         self.train = []
         self.val = []
@@ -32,6 +37,10 @@ class Dataset:
 
     @staticmethod
     def CreateDataset():
+        """
+        Creates a CSV file, with the entire dataset, divided into sequences.
+        :return: Dataframe containing the entire dataset.
+        """
         number_participants = 15
         number_trials = 24
         total_labels_df = pd.DataFrame(columns=['Frames Path', 'Frames Indexes', 'Class'])
@@ -101,15 +110,11 @@ class Dataset:
                 num_total_labels += len(total_trial)
 
                 # Sliding Window Method
-                window_length = 2
-                start_seq = 0
-                end_seq = start_seq + window_length
+                window_length = 4
 
-                for _ in range(len(total_trial) - 1):
-                    indexes = total_trial[start_seq:end_seq]
-
-                    indexes_df = [[indexes[0][0], indexes[1][0]]]
-                    next_label = indexes[1][1]
+                for i in range(window_length - 1, len(total_trial)):
+                    indexes_df = [[total_trial[i - 3][0], total_trial[i - 2][0], total_trial[i - 1][0], total_trial[i][0]]]
+                    next_label = total_trial[i][1]
 
                     part_trial = f'Participant_{participant + 1}\\Trial_{trial + 1}'
 
@@ -139,19 +144,16 @@ class Dataset:
                     # Concatenate all trial DataFrames for the participant
                     total_labels_df = total_labels_df.append(df, ignore_index=True)
 
-                    start_seq += 1
-                    end_seq += 1
-
         total_labels_df.to_excel(
-            r'C:\Users\diman\OneDrive\Ambiente de Trabalho\DATASET\RGB_labeling_30Hz_balanced_aligned_v3.xlsx',
+            r'C:\Users\diman\OneDrive\Ambiente de Trabalho\DATASET\RGB_labeling_30Hz_balanced_aligned_v4.xlsx',
             index=False)
 
         # Get number of frames
-        # with open(r'C:\Users\diman\OneDrive\Ambiente de Trabalho\DATASET\dataset_frames_count_v2.txt', 'w') as myFile:
-        # myFile.write(f"Number of frames '1': {num_frames_one}\n")
-        # myFile.write(f"Number of frames '-1': {num_frames_n_one}\n")
-        # myFile.write(f"Number of frames '0': {num_frames_zero}\n")
-        # myFile.write(f"Total number of frames: {num_total_labels}\n")
+        with open(r'C:\Users\diman\OneDrive\Ambiente de Trabalho\DATASET\dataset_frames_count_v5.txt', 'w') as myFile:
+            myFile.write(f"Number of frames '1': {num_frames_one}\n")
+            myFile.write(f"Number of frames '-1': {num_frames_n_one}\n")
+            myFile.write(f"Number of frames '0': {num_frames_zero}\n")
+            myFile.write(f"Total number of frames: {num_total_labels}\n")
 
         return total_labels_df
 
@@ -159,6 +161,16 @@ class Dataset:
     @staticmethod
     def crop_ROI(img, new_img_shape=(224, 224), height_leftcorner=60, height_rightcorner=450, width_leftcorner=85,
                  width_rightcorner=365):
+        """
+        Crops the image into the ROI (Region of Interest)
+        :param img: frame
+        :param new_img_shape: image shape
+        :param height_leftcorner: left corner
+        :param height_rightcorner: right corner
+        :param width_leftcorner: width of the left corner
+        :param width_rightcorner: width of the right corner
+        :return: cropped image
+        """
 
         new_img = np.zeros((new_img_shape[1], new_img_shape[0], img.shape[-1]))  # numpy: (height, width, channels)
         img = img[height_leftcorner:height_rightcorner, width_leftcorner:width_rightcorner]
@@ -213,8 +225,16 @@ class Dataset:
         return img
 
     def SplitDataset(self, num_participants, test_participants, val_participants):
+        """
+        Splits and saves the frames of the dataset into the train, val and test folders.
+        Each folder contains the respective participants, each with 24 trials.
+        :param num_participants: number of total participants
+        :param test_participants: number of test participants
+        :param val_participants: number of val participants
+        :return: indexes of train, val and test rows.
+        """
         dataset = pd.read_excel(os.path.join(self.dataset_path, self.excel_name))
-        path_labeling = r'D:\Labeling_v2'
+        path_labeling = r'D:\Labeling_v3'
 
         crop = True
         create_dirs = False
@@ -228,7 +248,7 @@ class Dataset:
 
         for dataset_ids, name in zip([test_participants, val_participants], ['test', 'val']):
             for subj_id in sorted(dataset_ids):
-                subj_path_name = 'participant' + subj_id
+                subj_path_name = 'Participant_' + subj_id
                 indexes = [ind for ind in range(dataset.shape[0]) if subj_path_name in dataset.iloc[ind, 0]]
                 test_val_indexes[name].extend(indexes)
                 train_indexes = train_indexes - set(indexes)
@@ -255,19 +275,20 @@ class Dataset:
         if create_dirs:
             for row in folder_part_df.items():
                 for participant in row[1]:
-                    os.makedirs(os.path.join(r'D:\Labeling_v2', row[0], f'Participant_{participant}'))
+                    os.makedirs(os.path.join(r'D:\Labeling_v3', row[0], f'Participant_{participant}'))
                     for trial in range(24):
-                        os.makedirs(os.path.join(r'D:\Labeling_v2',
+                        os.makedirs(os.path.join(r'D:\Labeling_v3',
                                                  row[0],
                                                  f'Participant_{participant}',
                                                  f'Trial_{trial + 1}'))
 
         all_indexes = {'train': train_indexes, 'val': test_val_indexes['val'], 'test': test_val_indexes['test']}
 
+        # LOOP THROUGH TRAIN, VAL AND TEST
         for item in all_indexes.items():
             indexes = item[1]
             count = 0
-
+            # LOOP THROUGH EACH PARTICIPANT OF TRAIN, VAL AND TEST
             for participants in folder_part_df.items():
                 for participant in participants[1]:
                     for trial in range(24):
@@ -276,10 +297,12 @@ class Dataset:
                         trial_list = []
                         first = True
                         trial_folder = f'Trial_{trial + 1}'
+                        # GET THE INDEXES FOR THE GIVEN TRIAL AND PARTICIPANT
                         for index in indexes:
-                            if (f'participant0{participant}\\' in dataset.iloc[
-                                index, 0] or f'participant{participant}\\' in dataset.iloc[
-                                    index, 0]) and trial_folder + '\\' in dataset.iloc[index, 0]:
+                            if ((f'Participant_{participant}\\' in dataset.iloc[
+                                index, 0] or f'Participant_0{participant}\\' in dataset.iloc[
+                                    index, 0]) and trial_folder in dataset.iloc[index, 0]
+                                    and len(trial_folder) == len(dataset.iloc[index, 0][dataset.iloc[index, 0].find(trial_folder):])):
                                 trial_list.append(index)
 
                         # Iterate over folders and sub-folders to find .avi files
@@ -306,7 +329,7 @@ class Dataset:
 
                         for idx in trial_list:
                             index_list = dataset.iloc[idx]['Frames Indexes'].split()
-
+                            # FIRST LOOP PRINTS ALL THE 4 FRAMES OF THE SEQUENCE, BUT DOES IT ONLY ONCE
                             if first:
                                 for i in index_list:
                                     frame_index = int(re.sub('\D', '', i))
@@ -325,7 +348,7 @@ class Dataset:
                                                              item[0],
                                                              f'Participant_{participant}',
                                                              f'Trial_{trial + 1}',
-                                                             f'{count}.jpg')
+                                                             f'{frame_index}.jpg')
                                     cv2.imwrite(frame_dir, resized_frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
                                     print(
                                         f'Image Saved: {frame_index}.png ; {trial + 1}/24 Trials - Participant {participant} ; {item[0]} folder')
@@ -334,29 +357,31 @@ class Dataset:
 
                                 first = False
 
+                            # PRINT THE index_list[1], index_list[2] AND index_list[3], FOR THE REST OF THE FRAMES
                             else:
-                                frame_index = int(re.sub('\D', '', index_list[1]))
-                                cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-                                ret, frame = cap.read()
-                                resized_frame = self.cut_original_img(frame, new_img_shape=(224, 224))
-                                if crop:
-                                    resized_frame = self.crop_ROI(resized_frame, new_img_shape=(224, 224))
-                                else:
-                                    resized_frame = cv2.resize(resized_frame, dsize=(224, 224),
-                                                               interpolation=cv2.INTER_AREA)
-                                    if len(resized_frame.shape) < len(frame.shape):
-                                        resized_frame = resized_frame[:, :, np.newaxis]
-                                count += 1
+                                for i in range(1, len(index_list)):
+                                    frame_index = int(re.sub('\D', '', index_list[i]))
+                                    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+                                    ret, frame = cap.read()
+                                    resized_frame = self.cut_original_img(frame, new_img_shape=(224, 224))
+                                    if crop:
+                                        resized_frame = self.crop_ROI(resized_frame, new_img_shape=(224, 224))
+                                    else:
+                                        resized_frame = cv2.resize(resized_frame, dsize=(224, 224),
+                                                                   interpolation=cv2.INTER_AREA)
+                                        if len(resized_frame.shape) < len(frame.shape):
+                                            resized_frame = resized_frame[:, :, np.newaxis]
+                                    count += 1
 
-                                frame_dir = os.path.join(path_labeling,
-                                                         item[0],
-                                                         f'Participant_{participant}\Trial_{trial + 1}',
-                                                         f'{count}.jpg')
-                                cv2.imwrite(frame_dir, resized_frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
-                                print(
-                                    f'Image Saved: {frame_index}.png ; {trial + 1}/24 Trials - Participant {participant} ; {item[0]} folder')
-                                print(f'{count}/193252 saved images.')
-                                print(f'{round((count / 193252) * 100)} % completed.')
+                                    frame_dir = os.path.join(path_labeling,
+                                                             item[0],
+                                                             f'Participant_{participant}\Trial_{trial + 1}',
+                                                             f'{frame_index}.jpg')
+                                    cv2.imwrite(frame_dir, resized_frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+                                    print(
+                                        f'Image Saved: {frame_index}.png ; {trial + 1}/24 Trials - Participant {participant} ; {item[0]} folder')
+                                    print(f'{count}/193252 saved images.')
+                                    print(f'{round((count / 193252) * 100)} % completed.')
 
                         # Release the video capture object and close windows
                         cap.release()
---
+++ GaitEventDetection/sequence_generator.py
@@ -0,0 +1,221 @@
+import os
+from keras.utils import Sequence
+import numpy as np
+import cv2
+import pandas as pd
+import re
+from sklearn.preprocessing import OneHotEncoder
+from models import DLModels
+import imutils
+from contrast_augm import contrast_shift, gaussBlur
+
+
+class ImageDataGenerator:
+    def __init__(self,
+                 input_shape,
+                 samplewise_center,
+                 samplewise_std_normalization,
+                 featurewise_center,
+                 featurewise_std_normalization,
+                 rotation_range,                            # done
+                 fill_mode,
+                 width_shift_range,
+                 height_shift_range,
+                 brightness_range,                          # done
+                 contrast_range,                            # done
+                 saturation_range,                          # done
+                 zoom_range,                                # done
+                 preprocessing_function,                    # done
+                 image_check_shape,                         # done
+                 gaussian_blur):                            # done
+
+        self.input_shape = input_shape
+        self.samplewise_center = samplewise_center
+        self.samplewise_std_normalization = samplewise_std_normalization
+        self.featurewise_center = featurewise_center
+        self.featurewise_std_center = featurewise_std_normalization
+        self.rotation_range = rotation_range
+        self.fill_mode = fill_mode
+        self.width_shift_range = width_shift_range
+        self.height_shift_range = height_shift_range
+        self.brightness_range = brightness_range
+        self.contrast_range = contrast_range
+        self.saturation_range = saturation_range
+        self.zoom_range = zoom_range
+        self.preprocessing_function = preprocessing_function
+        self.image_check_shape = image_check_shape
+        self.gaussian_blur = gaussian_blur
+
+    def image_correct_shape(self, image):
+        correct_height = correct_width = 224
+        # CORRECT IMAGE SHAPE IF NEEDED
+        if image.shape != self.input_shape:
+            cv2.resize(image, (correct_height, correct_width))
+        return image
+
+    def image_pre_processing(self, image):
+        image = self.image_check_shape(image)
+        return cv2.cvtColor(image, cv2.COLOR_RGB2BGR) / 255       # assuming that each image pyxel have max size of 8 bits
+
+    def image_rotation(self, image, rotation_range):
+        image = self.image_check_shape(image)
+        self.rotation_range = rotation_range
+
+        return imutils.rotate_bound(image, self.rotation_range)
+
+    def image_HSV(self, image):
+        image = self.image_check_shape(image)
+
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv_image)                          # h = hue (contrast); s = saturation; v = value (brightness)
+
+        if self.brightness_range != 0:
+            if self.brightness_range >= 0:                                        # => increase brightness
+                pixel_value_limit = 1 - self.brightness_range / 255               # pixel limit value (assuming 8 bit)
+                v[v > pixel_value_limit] = 1                                      # set every pixel that's greater than the limit to the max (8 bit = 255)
+                v[v <= pixel_value_limit] += self.brightness_range / 255          # add brightness_range to every pixel that's under the limit
+
+            else:                                                                 # the same, but if brightness_range is negative => decrease brightness.
+                pixel_value_limit = abs(self.brightness_range) / 255
+                v[v < pixel_value_limit] = 0
+                v[v >= pixel_value_limit] -= abs(self.brightness_range) / 255
+
+        elif self.saturation_range != 0:
+            if self.saturation_range >= 0:                                        # => increase saturation
+                pixel_value_limit = 1 - self.saturation_range / 255               # pixel limit value (assuming 8 bit)
+                s[s > pixel_value_limit] = 1                                      # set every pixel that's greater than the limit to the max (8 bit = 255)
+                s[s <= pixel_value_limit] += self.saturation_range / 255          # add brightness_range to every pixel that's under the limit
+
+            else:                                                                 # the same, but if brightness_range is negative => decrease saturation.
+                pixel_value_limit = abs(self.saturation_range) / 255
+                s[s < pixel_value_limit] = 0
+                s[s >= pixel_value_limit] -= abs(self.saturation_range) / 255
+
+        image = cv2.merge((h, s, v))
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+        if self.contrast_range != 0:
+            image = image * (self.contrast_range/127 + 1) - self.contrast_range + self.brightness_range
+            image = np.clip(image, 0, 1)
+            image = np.uint(image)
+
+        return image
+
+    def image_gaussian_blur(self, image):
+        image = self.image_check_shape(image)
+        kernel_size = 9
+
+        if self.gaussian_blur:
+            image = image.copy()
+            image = cv2.GaussianBlur(image, kernel_size)
+
+        return image
+
+    def image_zoom(self, image):
+        image = self.image_check_shape(image)
+
+        height, width = self.input_shape[2], self.input_shape[3]
+
+        new_height, new_width = int(height * self.zoom_range), int(width * self.zoom_range)
+
+        diff_height, diff_width = (new_height - height) / 2, (new_width - width) / 2
+
+        cropped_image = image[diff_height:height-diff_height, diff_width: width-diff_width]
+
+        image = cv2.resize(cropped_image, (width, height), interpolation=cv2.INTER_LINEAR)
+
+        return image
+
+
+class SequenceDataGenerator(Sequence):
+    def __init__(self, dataframe, batch_size):
+        self.dataframe = dataframe
+        self.batch_size = batch_size
+        self.n = len(self.dataframe)
+        self.contrast_gauss_normalize = DLModels.contrast_gauss_normalize
+        self.load_and_preprocess_image = DLModels.load_and_preprocess_image
+        self.aug_params = {"samplewise_center": False, "samplewise_std_normalization": False,
+                           "featurewise_center": False, "featurewise_std_normalization": False,
+                           "rotation_range": 0, "fill_mode": 'constant', "width_shift_range": 15.0,
+                           "height_shift_range": 15.0, "brightness_range": [0.75, 1.25], "zoom_range": 0.2,
+                           "preprocessing_function_train": self.contrast_gauss_normalize,
+                           "preprocessing_function": self.load_and_preprocess_image,
+                           "contrast": True, "blur": True}
+
+    @staticmethod
+    def splitTrainValTest(csv_file):
+        """
+        Splits a large dataset into train, val and test rows.
+        Useful to save time, by only splitting the dataset once, before the generator is called.
+        :param csv_file: dataset
+        :return: train, val and test DataFrames, containing only their rows in the dataset df.
+        """
+        df = pd.read_excel(csv_file)
+
+        mask_train = df['Frames Path'].str.contains('train')
+        train_df = df[mask_train]
+        mask_val = df['Frames Path'].str.contains('val')
+        val_df = df[mask_val]
+        mask_test = df['Frames Path'].str.contains('test')
+        test_df = df[mask_test]
+
+        return train_df, val_df, test_df
+
+    def __len__(self):
+        """
+        Get the number of batches.
+        :return: number of batches
+        """
+        return int(np.ceil(self.n / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        """
+        Get a determined batch (indicated by idx).
+        :param idx: index to the desired batch
+        :return: batch of images and labels.
+        """
+        path_images = []
+        batch_images = []
+
+        batch_rows_df = self.dataframe[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        batch_labels = batch_rows_df['Class'].tolist()
+
+        for row in batch_rows_df.index:
+            path_sequence = []
+            for idx in batch_rows_df.loc[row, 'Frames Indexes'].split():
+                idx = int(re.sub('\D', '', idx))
+                index_path = os.path.join(batch_rows_df.loc[row, 'Frames Path'].replace('v2', 'v3'), f'{str(idx)}.jpg')
+                path_sequence.append(index_path)
+            path_images.append(path_sequence)
+
+        for sequence in path_images:
+            images_sequence = [cv2.imread(image) for image in sequence]
+            # Perform additional preprocessing if needed, such as normalization
+            batch_images_proc = [ImageDataGenerator.image_pre_processing(image) for image in images_sequence]
+            batch_images.append(batch_images_proc)
+
+        # FAZER: ADICIONAR DATA AUGMENTATION AQUI DENTRO.
+        #    1º: VER EM ARTIGOS QUAL A PERCENTAGEM DE IMAGENS DO DATASET QUE COSTUMAM SER AUGMENTED.
+        #    2º: FAZER O MEU PRÓPRIO IMAGEDATAGENERATOR => COM BASE NAS FUNÇÕES DA IMAGEDATAGENERATOR, CRIAR E
+        #                                                  APLICAR MÉTODOS DE DATA AUGMENTATION.
+
+        batch_images = np.array(batch_images)               # shape: (64, 4, 224, 224, 3)
+
+        # Creating a numpy array from the list
+        batch_labels = np.array(batch_labels).reshape(-1, 1)
+
+        encoder = OneHotEncoder(sparse=False, categories=[[-1, 0, 1]])
+
+        # Fitting the encoder and transforming the labels into one-hot encoded format
+        batch_labels = encoder.fit_transform(batch_labels)  # shape: (64, 3), with [0 1 0] = 0, [1 0 0] = -1, [0 0 1] = 1
+
+        return batch_images, batch_labels
+
+    """193251 seqs / 64 = 3019.546874 ~ 3020 batch (grupos) de 64 batch size (tamanho dos grupos)
+                           ^
+                    3019 * 64 + 35 seqs
+
+        [img1   ,   img2], [img2, img3] ... = 64 imgs
+          ^          ^
+    (224, 224, 3) (224, 224, 3)"""
